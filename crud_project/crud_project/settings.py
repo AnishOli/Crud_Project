93eef9009dae733a40daefce6635e2c574b67a32
AnishOli <olianish534@gmail.com>
2026-02-12 11:36:57 +0545

    Update settings.py

--- crud_project/crud_project/settings.py
+++ crud_project/crud_project/settings.py
@@ -144,4 +144,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'anisholi751106@gmail.com'
-EMAIL_HOST_PASSWORD = 'hmtu rxqm okbd vccc'
+EMAIL_HOST_PASSWORD = #
